aabefc65635c80e608b3de839399f667bdfa7273
s1baeumer <andreasb@sentinelone.com>
2023-04-27 17:26:29 +0200

    test version check

--- scripts/deploy-latest-version.ps1
+++ scripts/deploy-latest-version.ps1
@@ -4,48 +4,65 @@
 
 
 param (
-    [Parameter(Mandatory=$false)][Boolean]$skipVersionCheck=$True,
+    [Parameter(Mandatory=$false)][Boolean]$skipVersionCheck=$False,
     [Parameter(Mandatory=$false)][String]$PWD="C:\Users\demo\Desktop",
     [Parameter(Mandatory=$false)][String]$REPO_URL="https://github.com/s1baeumer/remoteops-datacollection/blob/main/binaries/",
-    [Parameter(Mandatory=$false)][Boolean]$DebugLogging=$False
+    [Parameter(Mandatory=$false)][Boolean]$DebugLogging=$True
 )
 
+function Logging( $msg) {
+    if ($DebugLogging -eq $True) {
+        Write-Host $msg
+    } 
+}
+
+
+
+Logging "START SCRIPT"
 # TODO LOGGING TO XDR
 
 
 # DOWNLOAD VERSION FILE
-Invoke-WebRequest -Uri "https://github.com/s1baeumer/remoteops-datacollection/blob/main/binaries/versions" -OutFile $PWD"\versions.txt"
-$arrayFromFile = Get-Content -Path $PWD'\versions.txt'
+Logging "Trying to download versions file from repo"
 
-foreach ($data in $arrayFromFile) {
-    $d = $data -split '='
-    $bin = $d[0]
-    $ver = $d[1]
-    $path = $PWD+'\'+$bin
-  
-    $cver = (Get-Item $path).VersionInfo.FileVersion
-    if ($skipVersionCheck -ne $True) {
-        if ($ver -ne $cver) {
-            Write-Host "Version mismatch or file not existing - trying to download the correct binary from repo"
-            try {
-                Remove-Item $path
-                Invoke-WebRequest -Uri $REPO_URL$bin"?raw=true" -OutFile $PWD"\"$bin
-            } 
-            catch {
-                Write-Host "couldn't download $bin from repo" 
+try {
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/s1baeumer/remoteops-datacollection/main/binaries/versions" -OutFile $PWD"\versions.txt"
+    $arrayFromFile = Get-Content -Path $PWD'\versions.txt'
+
+    foreach ($data in $arrayFromFile) {
+        $d = $data -split '='
+        $bin = $d[0]
+        $ver = $d[1]
+        $path = $PWD+'\'+$bin
+        
+        if ($skipVersionCheck -ne $True) {
+            $cver = (Get-Item $path).VersionInfo.FileVersion
+            if ($ver -ne $cver) {
+                Logging "Version mismatch or file not existing - trying to download the correct binary from repo"
+                try {
+                    Logging "Removing binary $bin before new download"
+                    Remove-Item $path
+                    Logging "Download new binary"
+                    Invoke-WebRequest -Uri $REPO_URL$bin"?raw=true" -OutFile $PWD"\"$bin
+                } 
+                catch {
+                    Logging "couldn't download $bin from repo" 
+                }
+            } else {
+                Logging $path "- perfect match "
             }
         } else {
-            Write-Host $path "- perfect match "
+            Logging "Removing binary $bin before new download"
+            Remove-Item $path
+            Logging "Download new binary"
+            Invoke-WebRequest -Uri $REPO_URL$bin"?raw=true" -OutFile $PWD"\"$bin
         }
-    } else {
-        Remove-Item $path
-        Invoke-WebRequest -Uri $REPO_URL$bin"?raw=true" -OutFile $PWD"\"$bin
     }
 }
-Write-Host "FINISHED SCRIPT"
-
-
-
+catch {
+    Logging "Couldn't download version file from repo"
+}
+Logging "FINISHED SCRIPT"
 
 
 
